e7970e552d366df70f30d16519857f8130c87c66
CamMcCluskey <123849728+CamMcCluskey@users.noreply.github.com>
2023-05-15 14:31:59 -0700

    connecting to backend

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://stingray-app-jvmwj.ondigitalocean.app/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -78,7 +78,7 @@ class App extends Component {
     .then(response => response.json())
     .then(response => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://stingray-app-jvmwj.ondigitalocean.app/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
